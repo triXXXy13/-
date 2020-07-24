2c560bbf4caae16df06811d03042cb157a60473a
Nemo <hackersaheadddd@gmail.com>
2020-07-24 15:45:14 +0300

    Переводим Java Spring Boot приложение с библиотеки шаблонов Mustache на Freemarker

--- src/main/java/servingwebcontent/controller/MainController.java
+++ src/main/java/servingwebcontent/controller/MainController.java
@@ -5,6 +5,7 @@ package servingwebcontent.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,10 +26,16 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
         Iterable<Message> messages = messageRep.findAll();
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRep.findByTag(filter);
+        } else {
+            messages = messageRep.findAll();
+        }
 
-        model.put("messages", messages);
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
 
         return "main";
     }
@@ -49,18 +56,5 @@ public class MainController {
         return "main";
     }
 
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model) {
-        Iterable<Message> messages;
 
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRep.findByTag(filter);
-        } else {
-            messages = messageRep.findAll();
-        }
-
-        model.put("messages", messages);
-
-        return "main";
-    }
 }
